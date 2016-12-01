a1c65d08c45ee155e930f02554c06dea79e70030
Ryan James Spencer <spencer.ryanjames@gmail.com>
2016-12-02 09:03:39 +1100

    Add haskell project scaffolding

---
+++ 2016/Haskell/AdventOfCode/src/Day01.hs
@@ -0,0 +1,14 @@
+module Main where
+
+type Point = (Int, Int)
+
+origin :: Point
+origin = (0, 0)
+
+headings :: [Point]
+headings = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+
+
+main :: IO ()
+main = putStrLn "Not yet implemented"
