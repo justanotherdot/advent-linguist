7a8edba41dff4da8a307f5991449f6b90cd1842b
Ryan James Spencer <spencer.ryanjames@gmail.com>
2016-12-04 10:06:31 +1100

    Make correct solution by correctly parsing commands
    Did not think to parse beyond a single digit for the distance at each place

--- 2016/Haskell/AdventOfCode/src/Day01.hs
+++ 2016/Haskell/AdventOfCode/src/Day01.hs
@@ -1,11 +1,10 @@
 -- module Day01 (solveDay01) where
 module Day01 where
 
-import           Control.Monad   (mapM_)
+import           Data.Char       (isDigit)
 import           Data.Complex
 import           Data.Foldable   (foldl')
 import           Data.List.Split (splitOn)
-import           Text.Printf
 
 origin :: Complex Double
 origin = 0 :+ 0
@@ -18,7 +17,7 @@ commands = map processCommand
   where
     processCommand s = (dir, dist)
       where dir  = take 1 s
-            dist = (read (take 1 (drop 1 s)) :: Double) :+ 0
+            dist = (read (takeWhile isDigit (drop 1 s)) :: Double) :+ 0
 
 -- √-1
 i :: Complex Double
@@ -33,18 +32,14 @@ step (heading, point) ("L", dist) = (h', (h' * dist) + point)
   where h' = heading * i
 step _ _                          = error "Unrecognized command"
 
-processCommands :: [(String, Complex Double)] ->
-                   [(Complex Double, Complex Double)]
-processCommands = scanl step (north, origin)
+processCommands :: [(String, Complex Double)] -> Complex Double
+processCommands = snd . foldl' step (north, origin)
 
 calcAns :: Complex Double -> Double
 calcAns (x :+ y) = abs x + abs y
-
-pprint :: (Complex Double, Complex Double) -> IO ()
-pprint (a0 :+ b0, a1 :+ b1) = printf "Heading: %4f + %4fi, Position: %4f + %4fi\n" a0 b0 a1 b1
 
 solveDay01 :: FilePath -> IO ()
 solveDay01 pathToInput = do
   ls <- readFile pathToInput
   let cleanInput = splitOn ", " ls
-  mapM_ pprint $ processCommands . commands $ cleanInput
+  print $ calcAns . processCommands . commands $ cleanInput
