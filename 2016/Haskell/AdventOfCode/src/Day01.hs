2f2907c2406f2984dbc5681fd92371568740ec7d
Ryan James Spencer <spencer.ryanjames@gmail.com>
2016-12-04 12:10:21 +1100

    Cleanup day01pt1 soln with type synonyms

--- 2016/Haskell/AdventOfCode/src/Day01.hs
+++ 2016/Haskell/AdventOfCode/src/Day01.hs
@@ -8,13 +8,14 @@ import           Data.List.Split (splitOn)
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
 
-origin :: Complex Double
+type Point   = Complex Double
+type Instr   = String
+type Command = (Instr, Point)
+
+origin :: Point
 origin = 0 :+ 0
 
-north :: Complex Double
-north = 0 :+ 1
-
-commands :: [String] -> [(String, Complex Double)]
+commands :: [String] -> [Command]
 commands = map processCommand
   where
     processCommand s = (dir, dist)
@@ -22,40 +23,25 @@ commands = map processCommand
             dist = (read (takeWhile isDigit (drop 1 s)) :: Double) :+ 0
 
 -- √-1
-i :: Complex Double
+i :: Point
 i = 0 :+ 1
 
-step :: (Complex Double, Complex Double) ->
-        (String, Complex Double)         ->
-        (Complex Double, Complex Double)
+step :: (Point, Point) -> Command -> (Point, Point)
 step (heading, point) ("R", dist) = (h', (h' * dist) + point)
   where h' = heading * conjugate i
 step (heading, point) ("L", dist) = (h', (h' * dist) + point)
   where h' = heading * i
 step _ _                          = error "Unrecognized command"
 
-processCommands :: [(String, Complex Double)] -> Complex Double
+processCommands :: [Command] -> Point
 processCommands = snd . foldl' step (north, origin)
+  where north = 0 :+ 1
 
--- Setup for part2 solution.
-processCommands' :: [(String, Complex Double)] -> [(Complex Double, Complex Double)]
-processCommands' = scanl step (north, origin)
-
-calcAns :: Complex Double -> Double
+calcAns :: Point -> Double
 calcAns (x :+ y) = abs x + abs y
-
-sndPart :: [Complex Double]
-        -> Map (Double, Double) Bool
-        -> Complex Double
-sndPart []     _ = error "Never revisted any location"
-sndPart (p@(p1 :+ p2):ps) m = case M.lookup (p1, p2) m of
-                    Just _  -> p
-                    Nothing -> sndPart ps $ M.insert (p1, p2) True m 
 
 solveDay01 :: FilePath -> IO ()
 solveDay01 pathToInput = do
   ls <- readFile pathToInput
   let cleanInput = splitOn ", " ls
   print $ calcAns . processCommands . commands $ cleanInput
-  let points = map snd .processCommands' . commands $ cleanInput
-  print $ calcAns $ sndPart points M.empty
