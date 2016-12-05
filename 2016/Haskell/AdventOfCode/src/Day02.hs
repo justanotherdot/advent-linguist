af6c35bfbdfd1a1ca36160c20dc9782f6de759d1
Ryan James Spencer <spencer.ryanjames@gmail.com>
2016-12-05 13:00:08 +1100

    Provide solution for day02pt02

--- 2016/Haskell/AdventOfCode/src/Day02.hs
+++ 2016/Haskell/AdventOfCode/src/Day02.hs
@@ -4,34 +4,52 @@ import           Data.Foldable   (foldl')
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
 
-type KeypadPos = Integer
+type KeypadPos = Char
 type Command   = Char
 
 transitions :: Map (KeypadPos, Command) KeypadPos
-transitions = M.fromList [((1, 'R'), 2), ((1, 'D'), 4)
-                         ,((2, 'L'), 1), ((2, 'R'), 3), ((2, 'D'), 5)
-                         ,((3, 'L'), 2), ((3, 'D'), 6)
-                         ,((4, 'U'), 1), ((4, 'R'), 5), ((4, 'D'), 7)
-                         ,((5, 'U'), 2), ((5, 'R'), 6), ((5, 'D'), 8), ((5, 'L'), 4)
-                         ,((6, 'U'), 3), ((6, 'L'), 5), ((6, 'D'), 9)
-                         ,((7, 'U'), 4), ((7, 'R'), 8)
-                         ,((8, 'L'), 7), ((8, 'U'), 5), ((8, 'R'), 9)
-                         ,((9, 'L'), 8), ((9, 'U'), 6)]
+transitions = M.fromList [(('1', 'R'), '2'), (('1', 'D'), '4')
+                         ,(('2', 'L'), '1'), (('2', 'R'), '3'), (('2', 'D'), '5')
+                         ,(('3', 'L'), '2'), (('3', 'D'), '6')
+                         ,(('4', 'U'), '1'), (('4', 'R'), '5'), (('4', 'D'), '7')
+                         ,(('5', 'U'), '2'), (('5', 'R'), '6'), (('5', 'D'), '8'), (('5', 'L'), '4')
+                         ,(('6', 'U'), '3'), (('6', 'L'), '5'), (('6', 'D'), '9')
+                         ,(('7', 'U'), '4'), (('7', 'R'), '8')
+                         ,(('8', 'L'), '7'), (('8', 'U'), '5'), (('8', 'R'), '9')
+                         ,(('9', 'L'), '8'), (('9', 'U'), '6')]
 
-transition :: KeypadPos -> Command -> KeypadPos
-transition pos cmd = M.findWithDefault pos (pos, cmd) transitions
+transitions' :: Map (KeypadPos, Command) KeypadPos
+transitions' = M.fromList [(('1', 'D'), '3')
+                         ,(('9', 'L'), '8')
+                         ,(('D', 'U'), 'B')
+                         ,(('5', 'R'), '6')
+                         ,(('2', 'R'), '3'), (('2', 'D'), '6')
+                         ,(('4', 'L'), '3'), (('4', 'D'), '8')
+                         ,(('C', 'U'), '8'), (('C', 'L'), 'B')
+                         ,(('A', 'U'), '6'), (('A', 'R'), 'B')
+                         ,(('6', 'U'), '2'), (('6', 'R'), '7'), (('6', 'D'), 'A'), (('6', 'L'), '5')
+                         ,(('3', 'U'), '1'), (('3', 'R'), '4'), (('3', 'D'), '7'), (('3', 'L'), '2')
+                         ,(('8', 'U'), '4'), (('8', 'R'), '9'), (('8', 'D'), 'C'), (('8', 'L'), '7')
+                         ,(('B', 'U'), '7'), (('B', 'R'), 'C'), (('B', 'D'), 'D'), (('B', 'L'), 'A')
+                         ,(('7', 'U'), '3'), (('7', 'R'), '8'), (('7', 'D'), 'B'), (('7', 'L'), '6')]
 
-processLine :: KeypadPos -> [Command] -> KeypadPos
-processLine = foldl' transition
+transition :: Map (KeypadPos, Command) KeypadPos -> KeypadPos -> Command -> KeypadPos
+transition m pos cmd = M.findWithDefault pos (pos, cmd) m
 
-processLines :: [[Command]] -> String
-processLines = snd . foldl' go (5, "")
-  where go (pos, str) line = let pos' = processLine pos line
-                             in (pos', str ++ show pos')
+processLine :: Map (KeypadPos, Command) KeypadPos -> KeypadPos -> [Command] -> KeypadPos
+processLine m = foldl' (transition m)
+
+processLines :: Map (KeypadPos, Command) KeypadPos -> KeypadPos -> [[Command]] -> String
+processLines m start = snd . foldl' go (start, "")
+  where go (pos, str) line = let pos' = processLine m pos line
+                             in (pos', str ++ [pos'])
 
 solveDay02 :: FilePath -> IO ()
 solveDay02 path = do
   putStrLn "Solution for day two: "
   ls <- readFile path
-  putStrLn . processLines . lines $ ls
+  putStrLn "Part 1: "
+  putStrLn . processLines transitions '5' . lines $ ls
+  putStrLn "Part 2: "
+  putStrLn . processLines transitions' '5' . lines $ ls
   putStrLn ""
