83013dcab0d4030f4b47d325320f854357c1ae52
Ryan James Spencer <spencer.ryanjames@gmail.com>
2016-12-07 16:07:06 +1100

    Put dummy placeholder in for day05 to speed up execution

--- 2016/Haskell/AdventOfCode/app/Main.hs
+++ 2016/Haskell/AdventOfCode/app/Main.hs
@@ -4,7 +4,14 @@ import           Day01 (solveDay01)
 import           Day02 (solveDay02)
 import           Day03 (solveDay03)
 import           Day04 (solveDay04)
-import           Day05 (solveDay05)
+-- import           Day05 (solveDay05)
+
+solveDay05Dummy :: IO ()
+solveDay05Dummy = do
+  putStrLn "Solution for day five: "
+  putStrLn "f97c354d"
+  putStrLn "863dde27"
+  putStrLn ""
 
 main :: IO ()
 main = do
@@ -12,4 +19,5 @@ main = do
   solveDay02 "./input/day02.in"
   solveDay03 "./input/day03.in"
   solveDay04 "./input/day04.in"
-  solveDay05
+  -- solveDay05
+  solveDay05Dummy
