c8764842317e851f4f5ca3e4cffe177335814e8d
Ryan James Spencer <spencer.ryanjames@gmail.com>
2016-12-07 15:01:28 +1100

    More experimentation with day 05 parallelization
    Unfortunately, paralellizing day05pt01 (at least with the Par monad) is not
    ideal and is too costly in terms of gc and overhead that it's slower than
    the sequential variant.

--- 2016/Haskell/AdventOfCode/src/Day05.hs
+++ 2016/Haskell/AdventOfCode/src/Day05.hs
@@ -3,28 +3,27 @@
 
 module Day05 where
 
-#define PAR 1
+#define PAR 0
 
 #if PAR
 import           Control.Monad.Par
 #endif
 
 import           Crypto.Hash
-import           Data.ByteString.Char8 (pack)
+import           Data.ByteString.Lazy.Char8 (pack)
 import           Data.Monoid
 
 doorID :: String
 doorID = "reyedfim"
 
 md5 :: String -> Digest MD5
-md5 = hash . pack
+md5 = hashlazy . pack
 
 crackPassword :: String -> Integer -> String
 crackPassword s n
-  | n > (10^(7 :: Integer)) = s
-  -- | length s == 8 = s
+  | length s == 8 = s
   | otherwise     = if all (=='0') left
-                      then crackPassword (s <> [head right]) (n+1)
+                      then crackPassword (s <> take 1 right) (n+1)
                       else crackPassword s (n+1)
     where
       digest = show . md5 $ doorID <> show n
@@ -40,8 +39,11 @@ solveDay05 :: IO ()
 solveDay05 = do
   putStrLn "Solution for day five: "
 #if PAR == 0
-  print . reverse . crackPassword [] $ 0
+  print . crackPassword "" $ 0
 #else
-  print $ runPar $ parMapReduceRangeThresh 100 (InclusiveRange 1 (10^(7 :: Integer))) (\x -> return $ crackPassword' (fromIntegral x :: Integer)) (\x y -> return (x ++ y)) []
+  print $ runPar $ parMapReduceRangeThresh 897995
+                    (InclusiveRange 1 7183956)
+                    (\x -> return $ crackPassword' (fromIntegral x :: Integer))
+                    (\x y -> return (x <> y)) ""
 #endif
   putStrLn ""
