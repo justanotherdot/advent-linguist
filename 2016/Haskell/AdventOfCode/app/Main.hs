e79e2f2e8b6193a5a804ca684301b895e42fc9b8
Ryan James Spencer <spencer.ryanjames@gmail.com>
2016-12-07 13:38:24 +1100

    Make attempt at parallelizing day 05

--- 2016/Haskell/AdventOfCode/app/Main.hs
+++ 2016/Haskell/AdventOfCode/app/Main.hs
@@ -1,15 +1,15 @@
 module Main where
 
-import           Day01 (solveDay01)
-import           Day02 (solveDay02)
-import           Day03 (solveDay03)
-import           Day04 (solveDay04)
+-- import           Day01 (solveDay01)
+-- import           Day02 (solveDay02)
+-- import           Day03 (solveDay03)
+-- import           Day04 (solveDay04)
 import           Day05 (solveDay05)
 
 main :: IO ()
 main = do
-  solveDay01 "./input/day01.in"
-  solveDay02 "./input/day02.in"
-  solveDay03 "./input/day03.in"
-  solveDay04 "./input/day04.in"
+  -- solveDay01 "./input/day01.in"
+  -- solveDay02 "./input/day02.in"
+  -- solveDay03 "./input/day03.in"
+  -- solveDay04 "./input/day04.in"
   solveDay05
--- 2016/Haskell/AdventOfCode/src/Day05.hs
+++ 2016/Haskell/AdventOfCode/src/Day05.hs
@@ -1,4 +1,13 @@
+{-# LANGUAGE CPP               #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Day05 where
+
+#define PAR 1
+
+#if PAR
+import           Control.Monad.Par
+#endif
 
 import           Crypto.Hash
 import           Data.ByteString.Char8 (pack)
@@ -12,7 +21,8 @@ md5 = hash . pack
 
 crackPassword :: String -> Integer -> String
 crackPassword s n
-  | length s == 8 = s
+  | n > (10^(7 :: Integer)) = s
+  -- | length s == 8 = s
   | otherwise     = if all (=='0') left
                       then crackPassword (s <> [head right]) (n+1)
                       else crackPassword s (n+1)
@@ -20,8 +30,18 @@ crackPassword s n
       digest = show . md5 $ doorID <> show n
       (left, right) = splitAt 5 digest
 
+crackPassword' :: Integer -> String
+crackPassword' n = if all (=='0') left then take 1 right else ""
+    where
+      digest = show . md5 $ doorID <> show n
+      (left, right) = splitAt 5 digest
+
 solveDay05 :: IO ()
 solveDay05 = do
   putStrLn "Solution for day five: "
-  print $ crackPassword "" 0
+#if PAR == 0
+  print . reverse . crackPassword [] $ 0
+#else
+  print $ runPar $ parMapReduceRangeThresh 100 (InclusiveRange 1 (10^(7 :: Integer))) (\x -> return $ crackPassword' (fromIntegral x :: Integer)) (\x y -> return (x ++ y)) []
+#endif
   putStrLn ""
