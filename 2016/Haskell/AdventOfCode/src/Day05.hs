7d61e6aec0007f29fc7eb71be34d4848d828a618
Ryan James Spencer <spencer.ryanjames@gmail.com>
2016-12-07 15:54:36 +1100

    Provide solution for day05pt2

--- 2016/Haskell/AdventOfCode/src/Day05.hs
+++ 2016/Haskell/AdventOfCode/src/Day05.hs
@@ -3,14 +3,17 @@
 
 module Day05 where
 
-#define PAR 0
+#define PAR  0
 
 #if PAR
 import           Control.Monad.Par
 #endif
 
+import           Control.Arrow              (first)
 import           Crypto.Hash
 import           Data.ByteString.Lazy.Char8 (pack)
+import           Data.Function              (on)
+import           Data.List                  (sortBy)
 import           Data.Monoid
 
 doorID :: String
@@ -35,9 +38,29 @@ crackPassword' n = if all (=='0') left then take 1 right else ""
       digest = show . md5 $ doorID <> show n
       (left, right) = splitAt 5 digest
 
+crackPasswordPt2 :: [(String, String)] -> Integer -> [(String, String)]
+crackPasswordPt2 s n
+  | length s == 8 = s
+  | otherwise     = if all (=='0') left
+                    && head rightTwo `elem` ['0'..'7']
+                    && not (any (\x -> fst x == take 1 rightTwo) s)
+                      then let a = take 1 rightTwo
+                               b = take 1 (drop 1 rightTwo)
+                           in crackPasswordPt2 ((a,b) : s) (n+1)
+                      else crackPasswordPt2 s (n+1)
+    where
+      digest = show . md5 $ doorID <> show n
+      (left, right) = splitAt 5 digest
+      rightTwo = take 2 right
+
+constructPassword :: [(String, String)] -> String
+constructPassword ps = concat ps'
+  where ps' = map snd . sortBy (compare `on` fst) . map (first (read :: String -> Integer)) $ ps
+
 solveDay05 :: IO ()
 solveDay05 = do
   putStrLn "Solution for day five: "
+  putStrLn "Part one: "
 #if PAR == 0
   print . crackPassword "" $ 0
 #else
@@ -46,4 +69,6 @@ solveDay05 = do
                     (\x -> return $ crackPassword' (fromIntegral x :: Integer))
                     (\x y -> return (x <> y)) ""
 #endif
+  putStrLn "Part two: "
+  print . constructPassword . crackPasswordPt2 [] $ 0
   putStrLn ""
