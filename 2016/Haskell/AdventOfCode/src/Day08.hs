2e8cb55f11c5193c007dfa91ee1f1fe3f5903eb4
Ryan James Spencer <spencer.ryanjames@gmail.com>
2016-12-11 17:05:50 +1100

    Provide solution for day08pt01 and day08pt02

--- 2016/Haskell/AdventOfCode/src/Day08.hs
+++ 2016/Haskell/AdventOfCode/src/Day08.hs
@@ -1,8 +1,11 @@
 module Day08 where
 
-import           Data.List.Split (splitOn)
-import           Data.Vector     (Vector)
--- import qualified Data.Vector as V
+-- import           Control.Monad
+import           Control.Monad.Trans.State
+import           Data.List.Split           (chunksOf, splitOn)
+import           Data.Vector               (Vector, (!), (++))
+import qualified Data.Vector               as V
+import           Prelude                   hiding ((++))
 
 data Command = Rect Integer Integer
              | RotRow Integer Integer
@@ -32,23 +35,82 @@ toCommand s = case head parts of
                 _        -> error "Unrecognized command while parsing"
   where parts = words s
 
+pprintLCD :: LCDScreen -> IO ()
+pprintLCD lcd = mapM_ printIt chopped
+  where chopped = chunksOf 50 (V.toList lcd)
+        printIt s = do
+          mapM_ (\x -> if x then putStr "#" else putStr ".") s
+          putStrLn ""
 
 parseInput :: String -> [Command]
 parseInput s = map toCommand ls
   where ls = filter (not . null) . lines $ s
 
-rectOn :: LCDScreen -> Integer -> Integer -> LCDScreen
-rectOn _lcd _w _h = undefined
+rectOn :: Integer -> Integer -> State LCDScreen ()
+rectOn width height = do
+    lcd <- get
+    let lcd' = V.update lcd . V.fromList $ rectIxs width height
+    put lcd'
+  where
+    rectIxs :: Integer -> Integer -> [(Int, Bool)]
+    rectIxs w h
+      | w*h > 300  || w == 0 || h == 0 || w > 50 || h > 6 = []
+      | otherwise  = concatMap makeRow cols
+      where
+        cols = [0, 50 .. (h' * 50 -1)]
+        [w', h'] = map (\x -> fromIntegral x :: Int) [w, h]
+        makeRow offset = zip [(offset :: Int) .. (w' + offset - 1)] (repeat True)
 
-rotRow :: LCDScreen -> Integer -> Integer -> LCDScreen
-rotRow _lcd _row _amt = undefined
+rotateRight :: Int -> Int -> Vector a -> Vector a
+rotateRight amount upperBound vec = prefix ++ suffix
+  where prefix = V.take amount $ V.drop (upperBound - amount) vec
+        suffix = V.take (upperBound - amount) vec
 
-rotCol :: LCDScreen -> Integer -> Integer -> LCDScreen
-rotCol _lcd _col _amt = undefined
+rotRow :: Integer -> Integer -> State LCDScreen ()
+rotRow rowNo qty = do
+    lcd <- get
+    let lcd' = V.update lcd $ rotRowIxs lcd rowNo qty
+    put lcd'
+  where
+    rotRowIxs :: LCDScreen -> Integer -> Integer -> Vector (Int, Bool)
+    rotRowIxs vec row amt
+        | row >= 6 || row < 0 = V.empty
+        | otherwise = V.zip ixs vec'
+      where
+        row'   = fromIntegral row :: Int
+        amt'   = fromIntegral (amt `mod` 50) :: Int
+        ixs    = V.enumFromTo (row'*50) (row'*50+50-1)
+        rowVec = V.take 50 $ V.drop (row' * 50) vec
+        vec'   = rotateRight amt' 50 rowVec
+
+rotCol :: Integer -> Integer -> State LCDScreen ()
+rotCol colNo qty = do
+    lcd <- get
+    let lcd' = V.update lcd $ rotColIxs lcd colNo qty
+    put lcd'
+  where
+    rotColIxs :: LCDScreen -> Integer -> Integer -> Vector (Int, Bool)
+    rotColIxs vec col amt
+        | col >= 50 || col < 0 = V.empty
+        | otherwise = V.zip ixs vec'
+      where
+        col'   = fromIntegral col :: Int
+        amt'   = fromIntegral (amt `mod` 6) :: Int
+        ixs    = V.enumFromStepN col' 50 6
+        colVec = V.map (vec !) ixs
+        vec'   = rotateRight amt' 6 colVec
+
+processCommand :: Command -> State LCDScreen ()
+processCommand (Rect x y)     = rectOn x y
+processCommand (RotRow i amt) = rotRow i amt
+processCommand (RotCol j amt) = rotCol j amt
 
 solveDay08 :: FilePath -> IO ()
 solveDay08 path = do
   s <- readFile path
   putStrLn "Solution for day eight: "
-  mapM_ print $ parseInput s
+  let emptyLCD  = V.replicate (5*60) False
+  let filledLCD = execState (mapM_ processCommand $ parseInput s) emptyLCD
+  print . length . V.filter (==True) $ filledLCD
+  pprintLCD filledLCD
   putStrLn ""
