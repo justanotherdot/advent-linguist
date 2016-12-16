9ca8bc80d72c64687b9ed4af2eb484cbc54b6de3
Ryan James Spencer <spencer.ryanjames@gmail.com>
2016-12-16 15:27:02 +1100

    Supply more legwork in day10 solution

--- 2016/Haskell/AdventOfCode/src/Day10.hs
+++ 2016/Haskell/AdventOfCode/src/Day10.hs
@@ -1,11 +1,12 @@
 module Day10 where
 
 import           Control.Concurrent
+import           Control.Monad
 import           Data.Map           (Map)
--- import qualified Data.Map           as M
+import qualified Data.Map           as M
 
 type BotChan = Chan Int
-type BotDir  = Map Int BotChan
+type BotDir  = Map Int (BotChan, Id, OutputT, OutputT)
 data OutputT = Bot Int | Bin Int deriving (Show, Eq)
 type Id = Int
 
@@ -25,20 +26,43 @@ parseBotLine s = (bid, loType, hiType)
               "output" -> Bin hiVal
               _        -> error $ "Parse error: couldn't parse '" ++ s ++ "'"
 
-createBot :: BotChan -> String ->  IO (BotChan, Id, OutputT, OutputT)
-createBot ch s = do
+createBot :: String ->  IO (Id, (BotChan, Id, OutputT, OutputT))
+createBot s = do
+  ch <- newChan
   let (bid, lo, hi) = parseBotLine s
-  return (ch, bid, lo, hi)
+  return (bid, (ch, bid, lo, hi))
 
-respond :: BotChan -> OutputT -> OutputT -> IO ()
-respond ch _lo _hi = do
+spawnBot :: BotDir -> (BotChan, Id, OutputT, OutputT) -> IO ()
+spawnBot dir (ch, _bid, lo, hi) = void $! forkIO (respond dir ch lo hi)
+
+sendTo :: BotDir -> Int -> OutputT -> IO ()
+sendTo dir val (Bot bid) =
+  case M.lookup bid dir of
+    Just (ch, _, _, _) -> writeChan ch val
+    Nothing            -> error $ "Cannot find bot with that ID: " ++ show bid
+sendTo _ _ _ = undefined
+
+respond :: BotDir -> BotChan -> OutputT -> OutputT -> IO ()
+respond dir ch lo hi = do
   x <- readChan ch
   if x == 17 then print "hello" else print "nope"
+  respond dir ch lo hi
+
+parseValueLine :: String -> (Int, OutputT)
+parseValueLine s = (read (ws !! 1), out)
+  where ws  = words s
+        out = case ws !! 4 of
+                "bot"    -> Bot (read (ws !! 5))
+                "output" -> Bin (read (ws !! 5))
+                _        -> error $ "Parse error: couldn't parse " ++ show s
 
 solveDay10 :: FilePath -> IO ()
 solveDay10 path = do
   s <- readFile path
   let ls = lines s
+  bs <- M.fromList <$> traverse createBot (filter ((=='b') . head) ls)
+  void $! traverse (spawnBot bs) bs
+  let vs = map parseValueLine (filter ((=='v') . head) ls)
   putStrLn "Solution for day ten: "
-  print ls
+  mapM_ (uncurry $ sendTo bs) vs
   putStrLn ""
