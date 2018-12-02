15bde64976714c240b90445b378545538f32e484
Ryan James Spencer <spencer.ryanjames@gmail.com>
2018-12-02 12:30:27 +1100

    Soln day01_2018, pt02

--- 2018/day_01/src/internal.rs
+++ 2018/day_01/src/internal.rs
@@ -1,3 +1,5 @@
+use std::collections::{BTreeSet};
+
 #[derive(Debug)]
 enum OpTy {
     Add,
@@ -8,16 +10,47 @@ enum OpTy {
 pub fn day_01_pt_01(input: &String) -> isize {
     input.lines().into_iter()
         .map(parse_lines)
-        .fold(0, |acc, (op, num)| {
-            match op {
-                OpTy::Add =>
-                    acc + num,
-                OpTy::Subtract =>
-                    acc - num,
-                OpTy::Unknown =>
-                    acc,
+        .fold(0, |acc, (op, num)| run_op(op, acc, num))
+}
+
+pub fn day_01_pt_02(input: &String) -> (isize, bool) {
+    let mut seen_set = BTreeSet::new();
+    let mut acc = 0;
+
+    // could probably just use a scan here.
+    for (op, num) in input.lines().into_iter().cycle().map(parse_lines) {
+        let rv = match op {
+            OpTy::Add => {
+                acc += num;
+                acc
+            },
+            OpTy::Subtract => {
+                acc -= num;
+                acc
             }
-        })
+            OpTy::Unknown =>
+                acc
+        };
+
+        match seen_set.insert(rv) {
+            false => {
+                return (rv, true);
+            },
+            true => ()
+        }
+    }
+    (0, false)
+}
+
+fn run_op(op: OpTy, lhs: isize, rhs: isize) -> isize {
+    match op {
+        OpTy::Add =>
+            lhs + rhs,
+        OpTy::Subtract =>
+            lhs - rhs,
+        OpTy::Unknown =>
+            lhs,
+    }
 }
 
 fn parse_lines(line: &str) -> (OpTy, isize) {
--- 2018/day_01/src/main.rs
+++ 2018/day_01/src/main.rs
@@ -1,14 +1,17 @@
 mod internal;
-use internal::day_01_pt_01;
+use internal::*;
 use std::io::prelude::*;
 use std::fs::File;
 use std::env::{args};
 
 fn main() -> std::io::Result<()> {
-    let filename : String = args().skip(1).take(1).collect();
-    let pt_01_input_buffer = input_buffer(filename)?;
+    let filename_pt01 : String = args().skip(1).take(1).collect();
+    let pt_01_input_buffer = input_buffer(filename_pt01)?;
+    let filename_pt02 : String = args().skip(2).take(1).collect();
+    let pt_02_input_buffer = input_buffer(filename_pt02)?;
 
     println!("answer (pt. 01): {}", day_01_pt_01(&pt_01_input_buffer));
+    println!("answer (pt. 01): {:?}", day_01_pt_02(&pt_02_input_buffer));
 
     Ok(())
 }
