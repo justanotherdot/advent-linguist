8ea9c8bc59ec4ee11c0ea10a8f762984f6fe1606
Ryan James Spencer <spencer.ryanjames@gmail.com>
2018-12-06 21:14:46 +1100

    Parse timestamps into ISO8601

--- 2018/day_04/src/soln.rs
+++ 2018/day_04/src/soln.rs
@@ -3,17 +3,30 @@ extern crate regex;
 use self::regex::Regex;
 
 pub fn day_04(s: String) {
+    let mut v = vec![];
     for line in s.lines() {
-        parse_line(line);
+        v.push(parse_line(line));
     }
+    v.sort();
+    println!("{:?}", v);
 }
 
-fn parse_line(s: &str) {
+fn parse_line(s: &str) -> String {
     let re = Regex::new(
         r"\[(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2}) (?P<hour>\d{2}):(?P<minute>\d{2})\]"
     ).unwrap();
     let caps = re.captures(s).unwrap();
-    println!("{:?}", caps);
+    //println!("{:?}", caps);
+    let iso8601 = format!(
+        "{}-{:02}-{:02}T{:02}:{:02}:00Z",
+        &caps["year"],
+        &caps["month"],
+        &caps["day"],
+        &caps["hour"],
+        &caps["minute"],
+    );
+    //println!("{}", iso8601);
+    iso8601
 }
 
 #[cfg(test)]
